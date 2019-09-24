c8bb5df39f92a6ddb74a99600f0a0a16c634854f
iago gutierre <iago.msanto@gmail.com>
2019-09-24 13:10:34 -0300

    Ajustando marcações na page

--- page.php
+++ page.php
@@ -1,13 +1,16 @@
 <?php get_header();?>
-<?php if(have_posts()) :?>
-  <?php while(have_posts()) : the_post();?>
+<div class="main" style="height: 65%;">
+	<div class="container" >
+		<?php if(have_posts()) :?>
+		  <?php while(have_posts()) : the_post();?>
 
-    <?php get_template_part( 'template-parts/page/content', get_post_format() );?>
-  <?php endwhile; ?>
-<?php else : ?>
-  <h2>Página não encontrada</h2>
-<?php endif;?>
-
+		    <?php get_template_part( 'template-parts/page/content', get_post_format() );?>
+		  <?php endwhile; ?>
+		<?php else : ?>
+		  <h2>Página não encontrada</h2>
+		<?php endif;?>
+	</div>
+</div>
 
 
 <!-- footer vai arqui-->
